ec6664f0092333cbd1a660bbce277a2a8f46bdb0
Bhavya202 <76608764+Bhavya202@users.noreply.github.com>
2021-05-18 19:57:09 +0530

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the full form of HTML?",
+    answer: "Hypertext Markup Language",
+    options: [
+      "Hypertext Markup Language",
+      "Internet Site",
+      "World Web Wide Document",
+      "None Of The Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the full form of CSS?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "After Sales service",
+      "Client Service Software",
+      "Cascading Style Sheets",
+      "Correction For Style Software"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When was java launched?",
+    answer: "1996",
+    options: [
+      "1995",
+      "1996",
+      "1993",
+      "1994"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What was the most used programming language in 2017?",
+    answer: "JavaScript",
+    options: [
+      "HTML",
+      "CSS",
+      "PHP",
+      "JavaScript"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Full Form Of PHP is:-",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Personal Home Page",
+      "Private Hospital Priority",
+      "None Of The Above"
+    ]
+  },
+  {
+      numb: 6,
+      question: "When was PHP launched?",
+      answer: "1994",
+      options: [
+        "1996",
+        "1984",
+        "1994",
+        "2003"
+      ]
+  },
+  {
+      numb: 7,
+      question: "How many coding languages are there in the world?",
+      answer: "700",
+      options: [
+          "800",
+          "700",
+          "9000",
+          "1000"
+      ]
+  },
+  {
+      numb: 8,
+      question: "Which country is best in coding language?",
+      answer: "China",
+      options: [
+          "India",
+          "Switzerland",
+          "China",
+          "Russia"
+      ]
+  },
+  {
+    numb: 9,
+    question: "What is the best award in the field of computer science?",
+    answer: "Turing Award",
+    options: [
+        "ACM",
+        "Oscar Award",
+        "Golden Crow",
+        "Turing Award"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who developed Java?",
+    answer: "James Gosling",
+    options: [
+        "Brendan Eich",
+        "James Gosling",
+        "Guido van Rossum",
+        "Håkon Wium Lie"
+    ]
+  },
+];
